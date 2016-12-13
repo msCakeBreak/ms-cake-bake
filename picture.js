7669b34a6cb23586e53d2e91eaf0dab071196b72
Jose <jnavarro86@gmail.com>
2016-12-13 14:31:21 +0000

    footer

--- picture.js
+++ picture.js
@@ -3,15 +3,16 @@ var numberOfTags = 0;
   var addPhotos = function(tag) {
     $( "#photocontainer" ).append("<div class='grid' id='instafeed" + numberOfTags + "'><div class='grid-sizer'></div></div>" );
 
-    var imgTemplate = '<div class="grid-item"><img class="grid-img-fluid" src={{image}}/>'
-    + '<p class="imgDescription"><i class="fa fa-heart"></i> {{likes}} '
-    + '<i class="fa fa-comment"></i> {{comments}}</p></div>'
+    var imgTemplate = '<div class="grid-item"><a href={{link}} target="_blank"><img class="grid-img-fluid" src={{image}}/></a>'
+    + '<div class="footer"><div class="user"><a href="https://www.instagram.com/{{model.user.username}}/" target="_blank"><img src="{{model.user.profile_picture}}"></a>'
+    + '</div><div class="imgDescription"><p><i class="fa fa-heart"></i> {{likes}}</p>'
+    + '<p class="comment"><i class="fa fa-comment"></i> {{comments}}</p></div></div></div>'
 
     var feed = new Instafeed({
         get: 'tagged',
         tagName: tag,
         accessToken: '44511616.ba4c844.47b3e2cdf67141149c829d026e883d6a',
-        sortBy: 'most-recent',
+        sortBy: 'most-liked',
         // limit: 10,
         target: "instafeed" + numberOfTags,
         resolution: 'standard_resolution',
